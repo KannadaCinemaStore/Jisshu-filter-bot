c8230d4a0654c95770fe6ed2e241b5dc42a72442
KannadaCinemaStore <kannadacinemastore@gmail.com>
2025-01-23 16:04:49 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -115,7 +115,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/KannadaMagaa">𝙆𝙖𝙣𝙣𝙖𝙙𝙖𝙈𝙖𝙜𝙖𝙖</a>」</b>"""
+    FILE_CAPTION =""" 📂{file_caption}"""
     
 
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
